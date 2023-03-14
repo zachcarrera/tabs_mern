c20f3d2741edfa6340ae662f8a0da0210ef1fe68
Zach Carrera <zachcarrera@gmail.com>
2023-03-13 18:04:32 -0700

    add tab funtionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const oldTabs = [
+    {
+        label: "Tab 1",
+        content: "Tab 1 content is showing here...",
+        isActive: false,
+    },
+    {
+        label: "Tab 2",
+        content: "Tab 2 content is showing here...",
+        isActive: false,
+    },
+    {
+        label: "Tab 3",
+        content: "Tab 3 content is showing here...",
+        isActive: true,
+    },
+    {
+        label: "Tab 4",
+        content: "Tab 4 content is showing here...",
+        isActive: false,
+    },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tabs, setTabs] = useState(oldTabs);
+
+    const handleTabSwitch = (clickedIndex) => {
+        // use index to set that index in tabs to true
+
+        console.log("handle switch");
+
+        const newTabs = [...tabs];
+        // may need to map????
+
+        newTabs.forEach((tab) => {
+            if (tab.isActive) {
+                tab.isActive = false;
+            }
+        });
+
+        newTabs.forEach((tab, index) => {
+            if (index === clickedIndex) {
+                tab.isActive = true;
+            }
+        });
+
+        console.log(newTabs);
+
+        setTabs(newTabs);
+    };
+
+    return (
+        <div className="App">
+            {tabs.map((tab, index) => (
+                <button
+                    key={index}
+                    className={`tab-button ${tab.isActive ? "active" : ""} `}
+                    onClick={(e) => handleTabSwitch(index)}
+                >
+                    {tab.label}
+                </button>
+            ))}
+            {tabs
+                .filter((tab) => tab.isActive)
+                .map((tab, index) => (
+                    <p key={index} className="tab-content">
+                        {tab.content}
+                    </p>
+                ))}
+        </div>
+    );
 }
 
 export default App;
